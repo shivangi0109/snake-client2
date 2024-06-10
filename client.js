08998c3535760ba143e0d372314c739da6d83af5
Shivangi Srivastava <shi06dec@gmail.com>
2024-06-10 15:06:05 -0600

    separate module for the TCP connection in client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Handle Messages From Server
+  // Event handler for when the connection is established
+  // connect is a specific event that happens when a successful connection is made
+  conn.on("connect", () => {
+    console.log("Connected to the server");
+  });
+
+  // Handle Messages From Server
+  // Update the connect function to handle incoming data and console.log it for the player.
+  // Event handler for receiving data from the server
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
